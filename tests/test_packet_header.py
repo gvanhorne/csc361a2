223903a3cc204dbbff766f10d3ee4c13fb4ab0c9
Gregory Van Horne <gvanhorne@telus.net>
2023-11-01 20:09:34 -0700

    Add unit tests for classes

---
+++ tests/test_packet_header.py
@@ -0,0 +1,23 @@
+import unittest
+from packet_header import PacketHeader
+
+class TestPacketHeader(unittest.TestCase):
+    def test_from_bytes_valid_header(self):
+        # Valid packet header
+        header_bytes = b'\x8d\xad\xe7CX\xbb\x0c\x00>\x00\x00\x00>\x00\x00\x00'
+        packet_header = PacketHeader.from_bytes(header_bytes)
+
+        # Verify that the attributes are correctly parsed
+        self.assertEqual(packet_header.ts_sec, 0x43E7AD8D),
+        self.assertEqual(packet_header.ts_usec, 0xCBB58),
+        self.assertEqual(packet_header.incl_len, 62)
+        self.assertEqual(packet_header.orig_len, 62)
+
+    def test_from_bytes_invalid_header(self):
+        # Invalid packet header with incorrect length (should raise a ValueError)
+        header_bytes = b'\x8d\xad\xe7CX\xbb\x0c\x00>\x00\x00\x00>\x00\x00'  # Missing 4 bytes
+        with self.assertRaises(ValueError):
+            PacketHeader.from_bytes(header_bytes)
+
+if __name__ == '__main__':
+    unittest.main()
