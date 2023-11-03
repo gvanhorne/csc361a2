c43695bdf29f153d86695cb52c43cf90ec8b17ca
Gregory Van Horne <gvanhorne@telus.net>
2023-11-02 18:05:21 -0700

    Construct tcp header from packet

--- packet.py
+++ packet.py
@@ -5,8 +5,8 @@ from ethernet_header import EthernetHeader
 
 class Packet():
     #pcap_hd_info = None
-    IP_header = None
-    TCPHeader = None
+    ip_header = None
+    tcp_header = None
     timestamp = 0
     packet_No = 0
     RTT_value = 0
@@ -14,9 +14,9 @@ class Packet():
     buffer = None
     
     
-    def __init__(self):
-        self.IP_header = IPHeader()
-        self.TCPHeader = TCPHeader()
+    def __init__(self, ip_header, tcp_header):
+        self.ip_header = ip_header
+        self.tcp_header = tcp_header
         #self.pcap_hd_info = pcap_ph_info()
         self.timestamp = 0
         self.packet_No =0
@@ -47,3 +47,10 @@ class Packet():
         ip_header.get_total_len(packet_bytes[16:18])
         ip_header_bytes = packet_bytes[14:14+ip_header.ip_header_len]
         ip_header.get_IP(packet_bytes[26:30], packet_bytes[30:34])
+
+        tcp_header = TCPHeader()
+        tcp_header.get_data_offset(packet_bytes[14+ip_header.ip_header_len + 12:14+ip_header.ip_header_len + 13])
+        tcp_header_bytes = packet_bytes[14+ip_header.ip_header_len:14+ip_header.ip_header_len+tcp_header.data_offset]
+        tcp_header.get_src_port(packet_bytes[14+ip_header.ip_header_len:14+ip_header.ip_header_len + 2])
+        tcp_header.get_dst_port(packet_bytes[14+ip_header.ip_header_len + 2:14+ip_header.ip_header_len + 4])
+        return cls(ip_header, tcp_header)
